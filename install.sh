e9b30797ec948c9bc6e552871329656f8dadd495
Martin <138508073+hightesto@users.noreply.github.com>
2025-06-21 11:15:06 +0200

    Update install.sh
    added the real installer, check for sudo and also a check if i3 is installed, gonna do a check for more programs (like picom et cetera).

--- install.sh
+++ install.sh
@@ -1,1 +1,21 @@
 #!/bin/bash
+
+# bash check
+if [[ $EUID -ne 0 ]]; then
+   echo "This script has to be run with root (sudo)!"
+   exit 1
+fi
+
+# application check
+# add more
+if ! command -v i3 >/dev/null 2>&1
+then
+    echo "i3-wm could not be found"
+#    exit 1
+fi
+
+# now this is the real installation
+cd dotfiles
+cp -irvf config/* ~/.config
+cp -irvf etc/* /etc
+cp -irvf usr/* /usr
