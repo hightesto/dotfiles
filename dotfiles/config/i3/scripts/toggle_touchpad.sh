93f98b7a134db0e173ecde5e75820ac0980e0b5e
Martin <138508073+hightesto@users.noreply.github.com>
2025-06-28 19:42:32 +0200

    Update toggle_touchpad.sh
    improved code
    yes

--- dotfiles/config/i3/scripts/toggle_touchpad.sh
+++ dotfiles/config/i3/scripts/toggle_touchpad.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-STATE=$(xinput list-props 11 | grep "Device Enabled" | awk '{print $4}')
+STATE=$(xinput list-props "SYNA3602:00 0911:5288 Touchpad" | grep "Device Enabled" | awk '{print $4}')
 
-# i use 11 as touchpad id, run xinput list to find your id out
+# i use "SYNA3602:00 0911:5288 Touchpad" as touchpad name (as it is my actual touchpad lol), run xinput list to find out your touchpad name (you can alternatively use the id)
 
 if [ $STATE -eq 1 ]; then
-    xinput disable 11
+    xinput disable "SYNA3602:00 0911:5288 Touchpad"
     notify-send "Touchpad disabled"
 else
-    xinput enable 11
+    xinput enable "SYNA3602:00 0911:5288 Touchpad"
     notify-send "Touchpad enabled"
 fi
